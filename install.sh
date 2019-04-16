43d7dd743923079c6eae374d761a3bbd4d817449
DavidMChan <david@iridescent.io>
2019-04-16 19:05:26 +0000

    Added git configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ set -e
 
 # Install the bashrc file
 cp -a bashrc "${HOME}/.bashrc"
+
+# Configure GIT
+git config --global credential.helper store
+git config --global user.name "DavidMChan"
+git config --global user.email "david@iridescent.io"
+git config --global core.editor vim
+git config --global merge.tool vimdiff
